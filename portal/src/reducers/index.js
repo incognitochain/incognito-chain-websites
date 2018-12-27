44b7bca1ba798ba1c95d390117eece0c82305004
0xsirrush <thaibao@autonomous.nyc>
2018-12-27 12:03:30 +0700

    - update

--- portal/src/reducers/index.js
+++ portal/src/reducers/index.js
@@ -3,11 +3,13 @@ import AppRouter from './app/router';
 import Constant from './constant';
 import Auth from './auth/reducer';
 import LanguageSwitcher from './languageSwitcher/reducer'
+import ThemeSwitcher from './themeSwitcher/reducer'
 
 export default {
   App,
   AppRouter,
   Constant,
   Auth,
-  LanguageSwitcher
+  LanguageSwitcher,
+  ThemeSwitcher,
 };
