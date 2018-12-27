e49b384504c009591f2f2b5ddcc1d0cf413e6051
0xsirrush <thaibao@autonomous.nyc>
2018-12-27 09:40:39 +0700

    - update for block explorer

--- explorer/src/components/Tx.jsx
+++ explorer/src/components/Tx.jsx
@@ -13,47 +13,47 @@ class Tx extends React.Component {
   }
 
   render() {
-    const { tx } = this.props;
+    const {tx} = this.props;
 
     return (
       <table className="c-table">
         <tbody>
-          <tr>
-            <td>Version</td>
-            <td>{tx.Version}</td>
-          </tr>
-          <tr>
-            <td>Type</td>
-            <td>{tx.Type}</td>
-          </tr>
-          <tr>
-            <td>Fee</td>
-            <td>{tx.Fee}</td>
-          </tr>
-          <tr>
-            <td>Lock time</td>
-            <td>{tx.LockTime}</td>
-          </tr>
-          <tr>
-            <td>JSPubKey</td>
-            <td className="c-hash" style={{ wordWrap: 'break-word', whiteSpace: 'normal' }}>{tx.JSPubKey}</td>
-          </tr>
-          <tr>
-            <td>JSSig</td>
-            <td className="c-hash" style={{ wordWrap: 'break-word', whiteSpace: 'normal' }}>{tx.JSSig}</td>
-          </tr>
-          <tr>
-            <td>AddressLastByte</td>
-            <td>{tx.AddressLastByte}</td>
-          </tr>
-          <tr>
-            <td>Descs</td>
-            <td><pre>{JSON.stringify(tx.Descs, null, 4)}</pre></td>
-          </tr>
-          <tr>
-            <td>Metadata</td>
-            <td><pre>{tx.MetaData}</pre></td>
-          </tr>
+        <tr>
+          <td>Version</td>
+          <td>{tx.Version}</td>
+        </tr>
+        <tr>
+          <td>Type</td>
+          <td>{tx.Type}</td>
+        </tr>
+        <tr>
+          <td>Fee</td>
+          <td>{tx.Fee}</td>
+        </tr>
+        <tr>
+          <td>Lock time</td>
+          <td>{tx.LockTime}</td>
+        </tr>
+        <tr>
+          <td>SigPubKey</td>
+          <td className="c-hash" style={{wordWrap: 'break-word', whiteSpace: 'normal'}}>{tx.SigPubKey}</td>
+        </tr>
+        <tr>
+          <td>Sig</td>
+          <td className="c-hash" style={{wordWrap: 'break-word', whiteSpace: 'normal'}}>{tx.Sig}</td>
+        </tr>
+        <tr>
+          <td>Proof</td>
+          <td>
+            <pre>{JSON.stringify(tx.Proof, null, 4)}</pre>
+          </td>
+        </tr>
+        <tr>
+          <td>Metadata</td>
+          <td>
+            <pre>{tx.MetaData}</pre>
+          </td>
+        </tr>
         </tbody>
       </table>
     );
