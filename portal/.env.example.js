8a0bb7e41ce39987b30ef8db09e656b49acf4388
0xsirrush <thaibao@autonomous.nyc>
2018-12-31 13:30:50 +0700

    - create loan with eth web3

--- portal/.env.example.js
+++ portal/.env.example.js
@@ -1,3 +1,4 @@
 module.exports = {
   internalAPI: 'http://localhost:8000',
+  loanSmartContractAddress: '0x339f23F6844b17118338b24760AcbBBb309eDAF1',
 };
--- portal/src/pages/Create/index.jsx
+++ portal/src/pages/Create/index.jsx
@@ -7,6 +7,7 @@ import Portal from '../../services/portal'
 import Button from '@ui/uielements/button';
 import DatePicker from '@ui/uielements/datePicker'
 import moment from 'moment'
+import randomByte from 'random-bytes'
 
 class ComponentName extends React.Component {
   static propTypes = {
@@ -22,6 +23,7 @@ class ComponentName extends React.Component {
       collateralAmount: 0.0,
       loanAmount: 0,
       maturity: "",
+      secretKey: "",
       collateralType: "ETH",
     };
 
@@ -64,6 +66,7 @@ class ComponentName extends React.Component {
   }
 
   ethCallSmartContract() {
+    const {secretKey} = this.state
     if (typeof web3 !== 'undefined') {
       web3 = new Web3(web3.currentProvider);
     } else {
@@ -72,8 +75,12 @@ class ComponentName extends React.Component {
     }
     var abiDefinition = JSON.parse('[{"constant":false,"inputs":[{"name":"newPrice","type":"uint256"}],"name":"updateCollateralPrice","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"lid","type":"bytes32"},{"name":"digest","type":"bytes32"},{"name":"stableCoinReceiver","type":"bytes"},{"name":"request","type":"uint256"},{"name":"offchain","type":"bytes32"}],"name":"sendCollateral","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"value","type":"uint256"},{"name":"percent","type":"uint256"}],"name":"part","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"collateralAmount","type":"uint256"},{"name":"debtAmount","type":"uint256"}],"name":"collateralRatio","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"collateralPrice","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"lid","type":"bytes32"},{"name":"amount","type":"uint256"},{"name":"offchain","type":"bytes32"}],"name":"addPayment","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"lid","type":"bytes32"},{"name":"offchain","type":"bytes32"}],"name":"refundCollateral","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"collateralAmount","type":"uint256"},{"name":"debtAmount","type":"uint256"}],"name":"safelyCollateralized","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"lid","type":"bytes32"},{"name":"offchain","type":"bytes32"}],"name":"liquidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"name","type":"bytes32"}],"name":"get","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newPrice","type":"uint256"}],"name":"updateAssetPrice","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"name","type":"bytes32"},{"name":"value","type":"uint256"},{"name":"offchain","type":"bytes32"}],"name":"update","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"lender","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"loans","outputs":[{"name":"state","type":"uint8"},{"name":"borrower","type":"address"},{"name":"digest","type":"bytes32"},{"name":"amount","type":"uint256"},{"name":"request","type":"uint256"},{"name":"principle","type":"uint256"},{"name":"interest","type":"uint256"},{"name":"maturityDate","type":"uint256"},{"name":"escrowDeadline","type":"uint256"},{"name":"stableCoinReceiver","type":"bytes"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"assetPrice","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"lid","type":"bytes32"},{"name":"offchain","type":"bytes32"}],"name":"rejectLoan","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"params","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"lid","type":"bytes32"},{"name":"key","type":"bytes32"},{"name":"offchain","type":"bytes32"}],"name":"acceptLoan","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_lender","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"lid","type":"bytes32"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"offchain","type":"bytes32"}],"name":"__sendCollateral","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"lid","type":"bytes32"},{"indexed":false,"name":"key","type":"bytes32"},{"indexed":false,"name":"offchain","type":"bytes32"}],"name":"__acceptLoan","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"lid","type":"bytes32"},{"indexed":false,"name":"offchain","type":"bytes32"}],"name":"__rejectLoan","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"lid","type":"bytes32"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"offchain","type":"bytes32"}],"name":"__refundCollateral","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"lid","type":"bytes32"},{"indexed":false,"name":"offchain","type":"bytes32"}],"name":"__addPayment","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"lid","type":"bytes32"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"commission","type":"uint256"},{"indexed":false,"name":"offchain","type":"bytes32"}],"name":"__liquidate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"name","type":"bytes32"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"offchain","type":"bytes32"}],"name":"__update","type":"event"}]')
     var loanContract = web3.eth.contract(abiDefinition)
-    var contractInstance = loanContract.at('0x376eB6B3986FDe41fA4bCe7Bb0C0E6FD5a0e2Ba9');
-    contractInstance.sendCollateral("0x0", "abc", "1Uv3jP4ixNx3BkEtmUUxKXA1TXUduix3KMCWXHvLqVyA9CFfoLRZ949zTBNqDUPSzaPCZPrQKSfiEHguFazK6VeDmEk1RMLfX1kQiSqJ6", this.state.loanAmount * 100, "0x0",
+    var contractInstance = loanContract.at(process.env.loanSmartContractAddress);
+    var constantPaymentAddress = web3.fromAscii("1Uv3jP4ixNx3BkEtmUUxKXA1TXUduix3KMCWXHvLqVyA9CFfoLRZ949zTBNqDUPSzaPCZPrQKSfiEHguFazK6VeDmEk1RMLfX1kQiSqJ6")
+    var digestKey = web3.fromAscii(secretKey)
+    var lid = "0x0"
+    var offChain = randomBytes.sync(1)
+    contractInstance.sendCollateral(lid, digestKey, constantPaymentAddress, this.state.loanAmount * 100, offChain,
       {from: web3.eth.accounts[0]},
       function (a) {
         if (a !== undefined) {
@@ -128,14 +135,14 @@ class ComponentName extends React.Component {
     return time;
   }
 
-  changeLoanAmount(self, event) {
+  changeLoanAmount = (event) => {
     const val = event.target.value
-    const {choosenLoanParam} = self.state
+    const {choosenLoanParam} = this.state
     const LiquidationStart = choosenLoanParam.LiquidationStart
     if (LiquidationStart !== undefined) {
       var oracleRate = {'ETH': 118, 'BTC': 3674};
       var fix = {'ETH': 2, 'BTC': 10}
-      const collateralType = self.state.collateralType
+      const collateralType = this.state.collateralType
       var rate = oracleRate[collateralType];
       var collateralAmount = ((parseInt(val) / rate) * (LiquidationStart / 10000)).toFixed(fix[collateralType]);
       this.setState({
@@ -143,6 +150,13 @@ class ComponentName extends React.Component {
         loanAmount: parseInt(val),
       });
     }
+  }
+
+  changeSecretKey = (event) => {
+    const secretKey = event.target.value
+    this.setState({
+      secretKey: secretKey,
+    })
   }
 
   renderCollateralType() {
@@ -183,7 +197,7 @@ class ComponentName extends React.Component {
   }
 
   render() {
-    const {loanParams, collateralAmount, maturity, collateralType} = this.state;
+    const {loanParams, collateralAmount, maturity, collateralType, secretKey} = this.state;
     return (
       <div className="create">
         <div className="container">
@@ -204,7 +218,7 @@ class ComponentName extends React.Component {
                   </div>
                   <div className="col-12 col-md-4">
                     <h3>ENTER LOAN AMOUNT</h3>
-                    <Input onChange={(event) => this.changeLoanAmount(this, event)} type="number"></Input>
+                    <Input onChange={this.changeLoanAmount} type="number"></Input>
                   </div>
                   <div className="col-12 col-md-4">
                     <h3>COLLATERAL AMOUNT</h3>
@@ -212,12 +226,12 @@ class ComponentName extends React.Component {
                   </div>
                 </div>
                 <div className="row">
-                  {/*<div className="col-12 col-md-4">
+                  <div className="col-12 col-md-4">
                     <label>
-                      START DATE
-                      <DatePicker onChan/>
+                      Secret Key
+                      <Input type="password" value={secretKey} onChange={this.changeSecretKey}/>
                     </label>
-                  </div>*/}
+                  </div>
                   <div className="col-12 col-md-4">
                     <label>
                       Maturity
