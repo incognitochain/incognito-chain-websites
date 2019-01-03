fe63917aa51d4dbe95ee08e2de724cc5486c7961
0xbatutut <39509105+0xbatutut@users.noreply.github.com>
2019-01-03 13:21:49 +0700

    update app, router

--- portal/src/components/App/App.jsx
+++ portal/src/components/App/App.jsx
@@ -8,11 +8,6 @@ import Root from "@/components/App/Root";
 import '@/styles/main.scss';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return (
       <Provider store={store}>
--- portal/src/components/App/Root.jsx
+++ portal/src/components/App/Root.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import Router from '@/components/Router';
-import { withRouter } from 'react-router-dom';
 
 class Root extends React.Component {
-  static propTypes = {
-    // app: PropTypes.object.isRequired,
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
-    // const {app} = this.props;
     return (
       <Router />
     );
   }
 }
 
-export default withRouter(connect(state => ({ app: state.app }))(Root));
+export default Root;
