894bcd8c434b5f1477e54309df04c604ae13318f
Hakawai <vtkimsa@gmail.com>
2019-01-11 15:44:58 +0700

    remove hard code

--- exchange/src/containers/Sidebar/Sidebar.js
+++ exchange/src/containers/Sidebar/Sidebar.js
@@ -37,19 +37,6 @@ const stripTrailingSlash = str => {
   return str;
 };
 
-const subMenuVoting = (
-  <Menu>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
-    </Menu.Item>
-  </Menu>
-);
 
 const topMenus = [
   {
