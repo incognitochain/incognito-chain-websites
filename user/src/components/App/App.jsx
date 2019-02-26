70d46c6ef90cfd458349728c151b6169aa0d393e
0xarchibald <0xarchibald@>
2019-02-26 09:45:33 +0700

    add antd css

--- user/src/components/App/App.jsx
+++ user/src/components/App/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "store";
 import Root from "components/App/Root";
+
+import "antd/dist/antd.css";
 import "styles/main.scss";
 
 export default function App() {
