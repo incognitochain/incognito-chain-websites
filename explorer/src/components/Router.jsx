3d5d53bd6d768b0bb95931b5b5021617649d94c4
0xsirrush <thaibao@autonomous.nyc>
2019-03-06 11:16:18 +0700

    - update

--- explorer/src/components/Router.jsx
+++ explorer/src/components/Router.jsx
@@ -25,19 +25,71 @@ const Live = createDynamicImport(() => import('@/pages/Live'), Loading);
 const Info = createDynamicImport(() => import('@/pages/Info'), Loading);
 
 const routers = [
-  { path: '/', exact: true, component: Home },
-  { path: '/chains', exact: true, component: Chains },
-  { path: '/chain/:chainId/blocks', exact: true, component: Blocks },
-  { path: '/chain/:chainId', exact: true, component: Chain },
-  { path: '/block/:blockHash', exact: true, component: Block },
-  { path: '/block/:blockHash/txs', exact: true, component: Txs },
-  { path: '/txs/pending', exact: true, component: PendingTxs },
-  { path: '/tx/:txHash', exact: true, component: Tx },
-  { path: '/tokens', exact: true, component: Tokens },
-  { path: '/token/:customTokenId', exact: true, component: Token },
-  { path: '/committees', exact: true, component: Committees },
-  { path: '/live', exact: true, component: Live },
-  { path: '/info', exact: true, component: Info },
+  {
+    path: '/',
+    exact: true,
+    component: Home,
+  },
+  {
+    path: '/chains',
+    exact: true,
+    component: Chains,
+  },
+  {
+    path: '/chain/:chainId/blocks',
+    exact: true,
+    component: Blocks,
+  },
+  {
+    path: '/chain/:chainId',
+    exact: true,
+    component: Chain,
+  },
+  {
+    path: '/block/:blockHash',
+    exact: true,
+    component: Block,
+  },
+  {
+    path: '/block/:blockHash/txs',
+    exact: true,
+    component: Txs,
+  },
+  {
+    path: '/txs/pending',
+    exact: true,
+    component: PendingTxs,
+  },
+  {
+    path: '/tx/:txHash',
+    exact: true,
+    component: Tx,
+  },
+  {
+    path: '/tokens',
+    exact: true,
+    component: Tokens,
+  },
+  {
+    path: '/token/:customTokenId',
+    exact: true,
+    component: Token,
+  },
+  {
+    path: '/committees',
+    exact: true,
+    component: Committees,
+  },
+  {
+    path: '/live',
+    exact: true,
+    component: Live,
+  },
+  {
+    path: '/info',
+    exact: true,
+    component: Info,
+  },
 ];
 
 class Router extends React.Component {
--- explorer/src/pages/Chains.jsx
+++ explorer/src/pages/Chains.jsx
@@ -8,7 +8,7 @@ class Chains extends React.Component {
   static propTypes = {
     actionGetBlockChainInfo: PropTypes.func.isRequired,
     chainInfo: PropTypes.object.isRequired,
-  }
+  };
 
   constructor(props) {
     super(props);
@@ -45,31 +45,39 @@ class Chains extends React.Component {
               <div className="c-breadcrumb">
                 <ul>
                   <li><Link to="/">Explorer</Link></li>
-                  <li><Link to="/chains">Chain list</Link></li>
+                  <li><Link to="/chains">Shard list</Link></li>
                 </ul>
               </div>
             </div>
             <div className="col-12">
-              <div className="block"><h3 className="block-heading">Chain list</h3></div>
+              <div className="block"><h3 className="block-heading">Shard list</h3></div>
             </div>
           </div>
         </div>
         <div className="container">
           <div className="row">
-            {Object.values(bestBlocks).map((block, index) => (
-              <div className="col-12 col-sm-6 col-md-3 chain-item" key={block.Hash}>
-                <Link to={`/chain/${index + 1}`} className="card">
-                  <strong className="chain-id">{`Chain #${index + 1}`}</strong>
-                  <div className="chain-item-content">
-                    <div>{`Height: ${block.Height}`}</div>
-                    <div className="c-hash">{`Best block: ${block.Hash.substr(0, 5)}`}</div>
-                    <div>{`Total txs: ${block.TotalTxs}`}</div>
-                    {/* <div>{`Salary fund: ${block.SalaryFund}`}</div> */}
-                    <div>{`Salary per TX: ${block.SalaryPerTx}`}</div>
+            {Object.keys(bestBlocks)
+              .map((key) => {
+                if (key == -1) {
+                  return (<></>);
+                }
+                let index = parseInt(key);
+                let block = bestBlocks[key];
+                return (
+                  <div className="col-12 col-sm-6 col-md-3 chain-item" key={block.Hash}>
+                    <Link to={`/chain/${index + 1}`} className="card">
+                      <strong className="chain-id">{`Shard #${index + 1}`}</strong>
+                      <div className="chain-item-content">
+                        <div>{`Height: ${block.Height}`}</div>
+                        <div className="c-hash">{`Best block: ${block.Hash.substr(0, 5)}`}</div>
+                        <div>{`Total txs: ${block.TotalTxs}`}</div>
+                        <div>{`Salary fund: ${block.SalaryFund / 100}`}</div>
+                        <div>{`Salary per TX: ${block.SalaryPerTx}`}</div>
+                      </div>
+                    </Link>
                   </div>
-                </Link>
-              </div>
-            ))}
+                );
+              })}
           </div>
         </div>
       </div>
