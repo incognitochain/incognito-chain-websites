a04fe02b09d804e2a3184a5bdba5dcee9cda149b
Duc Hung <hung.han@notabasement.com>
2019-03-14 10:13:34 +0700

    update env example

--- .env.example.js
+++ .env.example.js
@@ -3,6 +3,7 @@ module.exports = {
   serviceWSAPI: 'ws://localhost:8888',
   blockchainAPI: 'http://localhost:9334',
   loanSmartContractAddress: '0x5e01c0870fAFB9d55C6e91e2B74Ae79e0ABf5a07',
+  reserveSmartContractAddress: '',
   domain: '.constant.money',
   userUrl: 'https://user-chain.constant.money',
   portalUrl: 'https://portal-chain.constant.money',
