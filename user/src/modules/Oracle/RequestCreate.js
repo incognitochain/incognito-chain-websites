b6141071e3c34ad6565b5e378d4ad83dc50483ff
Duc Hung <hung.han@notabasement.com>
2019-03-21 17:44:21 +0700

    update create request page

--- user/src/modules/Oracle/RequestCreate.js
+++ user/src/modules/Oracle/RequestCreate.js
@@ -7,9 +7,14 @@ import {
   TextField,
   TablePagination,
   FormControl,
+  Dialog,
+  DialogContent,
+  DialogContentText,
 } from '@material-ui/core';
 
 import Link from "components/Link";
+
+import { createAndSignMetadata } from "../../services/oracle";
 
 const mapStateToProps = (state) => {
   return {
@@ -23,7 +28,40 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class RequestCreate extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      pubkeys: "",
+      openDialog: false,
+      resultMessage: "",
+      isSubmitting: false,
+    }
+  }
+  onChangePubkey = (pubkeys = "") => {
+    this.setState({pubkeys});
+  }
+  onSubmit = async () => {
+    const {pubkeys=""} = this.state;
+    if (!pubkeys) return;
+    this.setState({isSubmitting: true})
+    const res = await createAndSignMetadata(pubkeys);
+    const {result, error} = res;
+    let resultMessage;
+    if (error) {
+      console.log(error)
+      resultMessage = error;
+    }
+    if (result || result === true) {
+      resultMessage = "Successfully";
+    }
+    this.setState({resultMessage, openDialog: true, isSubmitting : false})
+  }
+  onCloseDialog = () => {
+    this.setState({openDialog: false, resultMessage: ""})
+  }
   render() {
+    const {pubkeys, openDialog, isSubmitting} = this.state;
+    const isDisableButton = isSubmitting === true;
     return (
       <div className="page user-page home-page">
         <div className="container">
@@ -36,7 +74,7 @@ class RequestCreate extends React.Component {
                   </div>
                 </div>
                 <div className="row">
-                  <FormControl component="fieldset" >
+                  <FormControl component="fieldset" style={{width: "100%"}} >
                     <div className="title">PAYMENT ADDRESS</div>
                     <TextField
                       id="amount"
@@ -49,19 +87,32 @@ class RequestCreate extends React.Component {
                       InputProps={{
                         style: {paddingTop: 10, paddingBottom: 10, height:"inherit !important"},
                       }}
-                      // onChange={(e)=>this.onAmountChange(e.target.value)}
-                      // value={"abccccccccc"}
+                      onChange={(e)=>this.onChangePubkey(e.target.value)}
+                      value={pubkeys}
                     />
                   </FormControl>
                 </div>
                 <br/>
                 <div className="row">
                 <FormControl component="fieldset" >
-                  <button className="c-btn c-btn-primary submit"  style={{width: "100%"}} >
+                  <button className="c-btn c-btn-primary submit"  style={{width: "100%"}} onClick={this.onSubmit} disabled={isDisableButton ? true: false} >
                     Submit
                   </button>
                 </FormControl>
                 </div>
+
+                <Dialog
+                  open={openDialog}
+                  onClose={this.onCloseDialog}
+                  aria-labelledby="result-dialog-label"
+                  aria-describedby="result-dialog-description"
+                >
+                  <DialogContent>
+                    <DialogContentText>
+                      {this.state.resultMessage}
+                    </DialogContentText>
+                  </DialogContent>
+                </Dialog>
               </div>
             </div>
           </div>
