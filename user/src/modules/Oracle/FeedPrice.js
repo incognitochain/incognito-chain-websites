b7878bc6d0592003ec7d15ae6dd0829d4a885aa6
Duc Hung <hung.han@notabasement.com>
2019-03-22 11:53:26 +0700

    update oracle feed price page

--- user/src/modules/Oracle/FeedPrice.js
+++ user/src/modules/Oracle/FeedPrice.js
@@ -7,9 +7,16 @@ import {
   TextField,
   TablePagination,
   FormControl,
+  Select,
+  MenuItem,
+  Dialog,
+  DialogContent,
+  DialogContentText,
 } from '@material-ui/core';
 
 import Link from "components/Link";
+
+import { getAssets, feedPrice } from "../../services/oracle";
 
 const mapStateToProps = (state) => {
   return {
@@ -23,7 +30,58 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class FeedPrice extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      asset: "",
+      assets: [],
+      openDialog: false,
+      resultMessage: "",
+      price: "",
+    }
+  }
+  componentDidMount() {
+    this.onGetAssets();
+  }
+  onChangeAsset = (asset) => {
+    this.setState({asset})
+  }
+  onGetAssets = async () => {
+    const res = await getAssets();
+    const {result = [],error} = res;
+    if (error) {
+      console.log(error);
+    } else {
+      this.setState({assets: result})
+    }
+  }
+  onCloseDialog = () => {
+    this.setState({openDialog: false, resultMessage: ""})
+  }
+  onPriceChange = (price) => {
+    this.setState({price})
+  }
+  onSubmit = async () => {
+    const {asset="", price} = this.state;
+    if (!asset) return;
+    if (!price || isNaN(price)) return;
+    this.setState({isSubmitting: true})
+    const res = await feedPrice(parseFloat(price), asset);
+    const {result, error} = res;
+    let resultMessage;
+    if (error) {
+      console.log(error)
+      resultMessage = error;
+    }
+    if (result || result === true) {
+      resultMessage = "Successfully";
+    }
+    this.setState({resultMessage, openDialog: true, isSubmitting : false})
+  }
+
   render() {
+    const {assets=[], openDialog, isSubmitting} = this.state;
+    const isDisableBtn = isSubmitting === true;
     return (
       <div className="page user-page home-page">
         <div className="container">
@@ -32,14 +90,37 @@ class FeedPrice extends React.Component {
               <div className="c-card">
                 <div className="row">
                   <div className="hello">
-                    Fit Price
+                    Feed Price
                   </div>
                 </div>
                 <div className="row">
-                  <FormControl component="fieldset" >
+                  <FormControl component="fieldset" style={{width: "100%"}} >
+                    <div className="title">ASSETS</div>
+                    <Select
+                      value={this.state.asset}
+                      onChange={(e)=>this.onChangeAsset(e.target.value)}
+                      inputProps={{
+                        name: 'asset',
+                        id: 'asset',
+                      }}
+                    >
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      {assets && assets.length > 0 && assets.map((item={},i) => {
+                        return (
+                          <MenuItem key={`asset-${i}`} value={item.Asset}>{item.Name}</MenuItem>
+                        )
+                      })}
+                    </Select>
+                  </FormControl>
+                </div>
+                <br/>
+                <div className="row">
+                  <FormControl component="fieldset" style={{width: "100%"}} >
                     <div className="title">PRICE</div>
                     <TextField
-                      id="amount"
+                      id="price"
                       className="input-of-create cst"
                       type="number"
                       style={{
@@ -49,19 +130,32 @@ class FeedPrice extends React.Component {
                       InputProps={{
                         style: {paddingTop: 10, paddingBottom: 10, height:"inherit !important"},
                       }}
-                      // onChange={(e)=>this.onAmountChange(e.target.value)}
-                      // value={"abccccccccc"}
+                      onChange={(e)=>this.onPriceChange(e.target.value)}
+                      value={this.state.price}
                     />
                   </FormControl>
                 </div>
                 <br/>
                 <div className="row">
-                <FormControl component="fieldset" >
-                  <button className="c-btn c-btn-primary submit"  style={{width: "100%"}} >
-                    Submit
-                  </button>
-                </FormControl>
+                  <FormControl component="fieldset" >
+                    <button className="c-btn c-btn-primary submit"  style={{width: "100%"}} disabled={isDisableBtn ? true : false} onClick={this.onSubmit}>
+                      Submit
+                    </button>
+                  </FormControl>
                 </div>
+
+                <Dialog
+                  open={openDialog}
+                  onClose={this.onCloseDialog}
+                  aria-labelledby="result-dialog-label"
+                  aria-describedby="result-dialog-description"
+                >
+                  <DialogContent>
+                    <DialogContentText>
+                      {this.state.resultMessage}
+                    </DialogContentText>
+                  </DialogContent>
+                </Dialog>
               </div>
             </div>
           </div>
