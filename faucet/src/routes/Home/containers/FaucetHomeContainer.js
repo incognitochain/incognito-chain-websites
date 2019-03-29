47cd9e7759a90b62374148975e2d6ce49014ba1e
Duc Hung <hung.han@notabasement.com>
2019-03-29 15:58:07 +0700

    update layout

--- faucet/src/routes/Home/containers/FaucetHomeContainer.js
+++ faucet/src/routes/Home/containers/FaucetHomeContainer.js
@@ -29,7 +29,7 @@ const inlineStyle = {
     width: 24,
     height: 24,
     padding: 10,
-  } 
+  }
 }
 
 class FaucetHomePage extends React.Component {
@@ -73,25 +73,12 @@ class FaucetHomePage extends React.Component {
         <div className="App-header">
           <h2>Constant Authenticated Faucet </h2>
           <div style={{ minWidth: 800, display: "flex" }}>
-              {/* <TextField
-                id="outlined-full-width"
-                // label="Label"
-                style={{ margin: 8, color: 'white' }}
-                placeholder="Social network URL containing your Constant address"
-                fullWidth
-                margin="normal"
-                variant="outlined"
-                InputProps = {{
-                  color: "white"
-                }}
-                // InputLabelProps={{
-                //   shrink: true,
-                // }}
-              />*/}
 
               <Input
                 type="text"
                 fullWidth
+                placeholder="Social network URL containing your Constant address"
+                disableUnderline
                 style={{
                   padding: 5,
                   color: "white",
@@ -115,17 +102,6 @@ class FaucetHomePage extends React.Component {
               </Button>
           </div>
 
-          {/*<div>
-            <Paper elevation={1}>
-              <Typography variant="h5" component="h4">
-                This is a sheet of paper.
-              </Typography>
-              <Typography component="p">
-                Paper can be used to build surface or other elements for your application.
-              </Typography>
-            </Paper>
-          </div> */}
-
           <div style={{display: "flex", textAlign: "left", justifyContent: "center", marginTop: 30, fontSize: 14}}>
 
               <Grid item xs={8}>
@@ -138,7 +114,7 @@ class FaucetHomePage extends React.Component {
                     <path fill="#fff" d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" />
                   </svg>
 
-                  <div> 
+                  <div>
                     To request funds via Twitter, make a <a style={{color: "#bfbfce"}} href="https://twitter.com/intent/tweet?text=0x0000000000000000000000000000000000000000." target="_about:blank"> tweet</a> with your Constant address pasted into the contents (surrounding text doesn't matter).
                     <br/>Copy-paste the <a style={{color: "#bfbfce"}} href="https://support.twitter.com/articles/80586" target="_about:blank">tweets URL</a> into the above input box and fire away!
                   </div>
@@ -148,12 +124,12 @@ class FaucetHomePage extends React.Component {
                   <svg style={inlineStyle.socialIconStyle} viewBox="0 0 24 24">
                     <path fill="#fff" d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" />
                   </svg>
-                  
-                  <div> 
+
+                  <div>
                     To request funds via Facebook, publish a new <strong>public</strong> post with your Constant address embedded into the content (surrounding text doesn't matter).<br/>Copy-paste the <a style={{color: "#bfbfce"}} href="https://www.facebook.com/help/community/question/?id=282662498552845" target="_about:blank">posts URL</a> into the above input box and fire away!
                   </div>
                 </div>
-                  
+
 
                 <p>You can track the current pending requests below the input field to see how much you have to wait until your turn comes.</p>
               </Grid>
