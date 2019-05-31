8496d7d6d410e46ba12605d1f3aa31f330601f30
sirrush <thaibao@autonomous.nyc>
2019-05-31 11:03:59 +0700

    - get list candidate

--- explorer/src/components/Header.jsx
+++ explorer/src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {showDialog} from '@/reducers/app/action';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { showDialog } from '@/reducers/app/action';
 // import logoC from '@/assets/logo-C.svg.raw';
 import RootHeader from './RootHeader';
 
 class Header extends React.Component {
-  static propTypes = {}
+  static propTypes = {};
 
   constructor(props) {
     super(props);
@@ -26,6 +26,7 @@ class Header extends React.Component {
                 <div className="menu">
                   <ul className="c-list-inline">
                     <li><Link to="/">Home</Link></li>
+                    <li><Link to="/committees">Commitees</Link></li>
                     <li><Link to="/chain/0">Beacon Chain</Link></li>
                     <li><Link to="/chains">Shards</Link></li>
                     <li><Link to="/txs/pending">Pending TXs</Link></li>
@@ -42,6 +43,9 @@ class Header extends React.Component {
 }
 
 export default connect(
-  state => ({auth: state.auth}),
-  dispatch => ({appShowDialog: showDialog, dispatch}),
+  state => ({ auth: state.auth }),
+  dispatch => ({
+    appShowDialog: showDialog,
+    dispatch
+  }),
 )(Header);
