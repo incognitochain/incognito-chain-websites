2bb04fdda75e51e36d2dd340c9167d7e54476883
0xsirrush <thaibao@autonomous.nyc>
2019-10-01 12:10:19 +0700

    - show data

--- explorer/src/pages/Committees.jsx
+++ explorer/src/pages/Committees.jsx
@@ -4,13 +4,14 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {isEmpty} from 'lodash';
 import cn from '@sindresorhus/class-names';
-import {getListCommittee, getListRewardAmount} from '@/reducers/constant/action';
+import {getListCommittee, getListRewardAmount, getPrivacyTokens} from '@/reducers/constant/action';
 import {formatBlocksHeight, formatCoinValue} from '@/services/formatter';
 
 class Committees extends React.Component {
   static propTypes = {
     actionGetListCommittee: PropTypes.func.isRequired,
     actionGetListRewardAmount: PropTypes.func.isRequired,
+    actionGetPrivacyTokens: PropTypes.func.isRequired,
   };
 
   constructor(props) {
@@ -19,21 +20,29 @@ class Committees extends React.Component {
     this.state = {
       committees: props.committees,
       commiteesRewardAmount: props.commiteesRewardAmount,
+      privacyTokens: props.privacyTokens,
     };
   }
 
   async componentDidMount() {
-    const {actionGetListCommittee, actionGetListRewardAmount} = this.props;
+    const {actionGetListCommittee, actionGetListRewardAmount, actionGetPrivacyTokens} = this.props;
     await actionGetListCommittee();
     await actionGetListRewardAmount();
+    await actionGetPrivacyTokens();
   }
 
   render() {
-    const {committees, commiteesRewardAmount} = this.props;
-    if (!committees || !commiteesRewardAmount) {
+    const {committees, commiteesRewardAmount, privacyTokens} = this.props;
+    if (!committees || !commiteesRewardAmount || !privacyTokens) {
       return null;
     }
+
     const PRV = '0000000000000000000000000000000000000000000000000000000000000004'
+    var mapPrivacyTokens = {}
+    privacyTokens.list.forEach(function (item, i) {
+      mapPrivacyTokens[item.ID] = item.Symbol;
+    })
+    // mapPrivacyTokens[PRV] = "PRV";
 
     return (
       <div className="c-explorer-page c-explorer-page-tx">
@@ -71,6 +80,7 @@ class Committees extends React.Component {
                     <th>Mining Key in base58check.encode</th>
                     <th>Reward receiver in base58check.encode</th>
                     <th>PRV Reward</th>
+                    <th>Token Reward</th>
                   </tr>
                   </thead>
                   <tbody>
@@ -81,7 +91,29 @@ class Committees extends React.Component {
                           <td>{`${index + 1}`}</td>
                           <td className="c-hash">{value}</td>
                           <td className="c-hash">{committees.BeaconRewardReceiver[index]}</td>
-                          <td className="c-hash">{formatCoinValue(commiteesRewardAmount[committees.BeaconRewardReceiver[index]] ? commiteesRewardAmount[committees.BeaconRewardReceiver[index]][PRV] / 1e9 : 0)}</td>
+                          <td
+                            className="c-hash">{formatCoinValue(commiteesRewardAmount[committees.BeaconRewardReceiver[index]] ? commiteesRewardAmount[committees.BeaconRewardReceiver[index]][PRV] / 1e9 : 0)}
+                          </td>
+                          <td className="c-hash">
+                            {
+                              Object.keys(mapPrivacyTokens).map((key, i) => {
+                                var name = mapPrivacyTokens[key];
+                                if (commiteesRewardAmount[committees.BeaconRewardReceiver[index]] && commiteesRewardAmount[committees.BeaconRewardReceiver[index]][key]) {
+                                  var value = commiteesRewardAmount[committees.BeaconRewardReceiver[index]][key]
+                                  if (key == PRV) {
+                                    value = value / 1e9;
+                                  } else {
+                                    value = value / 1e9;
+                                  }
+                                  return (
+                                    <span>{name + ":" + value}</span>
+                                  );
+                                } else {
+                                  return <></>;
+                                }
+                              })
+                            }
+                          </td>
                         </tr>
                       ))
                       : (
@@ -116,6 +148,7 @@ class Committees extends React.Component {
                             <th>Mining Key in base58check.encode</th>
                             <th>Reward receiver in base58check.encode</th>
                             <th>PRV Reward</th>
+                            <th>Token Reward</th>
                           </tr>
                           </thead>
                           <tbody>
@@ -126,7 +159,29 @@ class Committees extends React.Component {
                                   <td>{`${index + 1}`}</td>
                                   <td className="c-hash">{value}</td>
                                   <td className="c-hash">{shardRewardReceiver[index]}</td>
-                                  <td className="c-hash">{formatCoinValue(commiteesRewardAmount[shardRewardReceiver[index]] ? commiteesRewardAmount[shardRewardReceiver[index]][PRV] / 1e9 : 0)}</td>
+                                  <td
+                                    className="c-hash">{formatCoinValue(commiteesRewardAmount[shardRewardReceiver[index]] ? commiteesRewardAmount[shardRewardReceiver[index]][PRV] / 1e9 : 0)}
+                                  </td>
+                                  <td className="c-hash">
+                                    {
+                                      Object.keys(mapPrivacyTokens).map((key, i) => {
+                                        var name = mapPrivacyTokens[key];
+                                        if ((commiteesRewardAmount[shardRewardReceiver[index]] && commiteesRewardAmount[shardRewardReceiver[index]][key])) {
+                                          var value = commiteesRewardAmount[shardRewardReceiver[index]][key]
+                                          if (key == PRV) {
+                                            value = value / 1e9;
+                                          } else {
+                                            value = value / 1e9;
+                                          }
+                                          return (
+                                            <span>{name + ":" + value}</span>
+                                          );
+                                        } else {
+                                          return <></>;
+                                        }
+                                      })
+                                    }
+                                  </td>
                                 </tr>
                               ))
                               : (
@@ -154,9 +209,11 @@ export default connect(
   state => ({
     committees: state.constant.commitees,
     commiteesRewardAmount: state.constant.commiteesRewardAmount,
+    privacyTokens: state.constant.privacyTokens,
   }),
   ({
     actionGetListCommittee: getListCommittee,
     actionGetListRewardAmount: getListRewardAmount,
+    actionGetPrivacyTokens: getPrivacyTokens,
   }),
 )(Committees);
