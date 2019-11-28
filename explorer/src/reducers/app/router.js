e4a854c98b39391eca2c20372dea7aba5ffe2443
0xday <hoai@incognito.org>
2019-11-28 10:44:21 +0700

    update metadata

--- explorer/src/reducers/app/router.js
+++ explorer/src/reducers/app/router.js
@@ -1,7 +1,6 @@
 export default (state = {}, action) => {
   switch (action.type) {
     case '@@router/LOCATION_CHANGE': {
-      document.title = 'Incognito Explorer';
       return state;
     }
     default: {
